6e1fd1bf95e0a6f9811c8c1687063b2ed6a552e1
김동혁 <donghyeok7561@gmail.com>
2026-04-11 20:57:55 +0900

    dft 초안

--- 3_DFT/0_script/DFT_script.tcl
+++ 3_DFT/0_script/DFT_script.tcl
@@ -1,8 +1,9 @@
 set ver $env(ver)
 
-source -e -v ./.synopsys_dc.setups
 
-read_verilog ../2_Synthesis/2_Output/$ver/mapped/ORCA_gate.v
+source -e -v ../.synopsys_dc.setup
+
+read_file -format ddc ../2_synthesis/2_output/${ver}/mapped/soc_gate.ddc
 current_design top_mcu_pll_sram_multiclk_soc
 link 
 
@@ -11,7 +12,7 @@ set test_default_bidir_delay 0
 set test_default_delay 0
 set test_default_strobe 40
 
-set test_enable_dft_drc
+set test_enable_dft_drc true
 set test_stil_netlist_format verilog 
 
 set_auto_disable_drc_nets -scan true
@@ -24,7 +25,7 @@ set power_cg_auto_identify true
 set_dft_configuration -connect_clock_gating enable
 set_dft_clock_gating_pin [get_cells u_ctrl/u_icg_aes]\
     -pin_name test_en\
-    -control_signal scan-enable
+    -control_signal ScanEnable
 
 #  latch 셀들을 scan chain element로 취급하지 말라는 뜻
 set_scan_element false [get_cells u_ctrl/u_icg_aes] 
@@ -35,5 +36,65 @@ set_dft_insertion_configuration -synthesis_optimization none\
     -preserve_design_name true
 
 # Synopsys가 삽입하는 DFT 전용 로직을 어느 계층/이름 아래 둘지 지정
-set_dft_location top_mcu_pll_sram_multiclk_soc/U_SNPS_DFT_LOGIC
+#set_dft_location top_mcu_pll_sram_multiclk_soc/U_SNPS_DFT_LOGIC
 
+set_dft_signal -view exist -type ScanClock  -port ref_clk   -timing {45 55}
+set_dft_signal -view exist -type ScanEnable -port scan_en   -active_state 1 
+set_dft_signal -view exist -type Reset      -port rst_n     -active_state 0
+
+#set_dft_signal -view exist -type TestMode -active_state 1 -port test_mode -view existing_dft
+
+# scan_in, scan_out
+set_dft_signal -view spec  -type ScanDataIn     -port scan_in
+set_dft_signal -view spec  -type ScanDataOut    -port scan_out
+
+set_scan_path chain0 -view spec -scan_data_in scan_in -scan_data_out scan_out
+
+#set_dft_configuration -scan_compression enable
+
+#scan chain 개수 1개
+set_scan_configuration -test_mode all -chain_count 1
+#scan FF 스타일을 muxed scan flop으로 
+set_scan_configuration -test_mode all -style multiplexed_flip_flop
+#internal clock를 별도로 scan clock로 쓰지 않음
+set_scan_configuration -test_mode all -internal_clocks none
+
+set_dft_configuration -fix_bus enable
+
+#clock, reset 수정 X
+set auto_fix 0
+
+#DFT 삽입 전에 test protocol을 생성하고, DFT rule check와 scan 구조확인
+create_test_protocol
+dft_drc                             > ./4_report/${ver}/pre_dft.rpt
+dft_drc -verbose                    > ./4_report/${ver}/pre_drc_verbose.rpt
+preview_dft -show scan_summary      > ./4_report/${ver}/preview_dft.rpt
+
+#DFT/합성 과정에서 새로 삽입되는 인스턴스 이름 앞에 DFTC_를 붙임
+set compile_instance_name_prefix DFTC_
+
+insert_dft
+
+current_test_mode Internal_scan
+dft_drc -verbose    > ./4_report/${ver}/insert_drc_internal.dft
+
+# Change names
+change_names -rules verilog -hierarchy
+
+write_test_protocol -test_mode Internal_scan\
+    -out ./2_output/${ver}/scan_internal.spf\
+    -names verilog
+
+write_scan_def -out ./2_output/${ver}/scan.def
+
+write -f verilog  -hierarchy -output ./2_output/${ver}/aes_128_internal.v
+write_file -f ddc -hierarchy -output ./2_output/${ver}/aes_128_internal.ddc
+
+current_test_mode Internal_scan
+
+report_scan_configuration                       >  ./4_report/${ver}/scan_config_internal.rpt
+report_dft_signal -view exist                   >  ./4_report/${ver}/dft_signals_internal.rpt
+report_dft_signal -view spec                    >> ./4_report/${ver}/dft_signals_internal.rpt
+report_scan_path -view existing_dft -chain all  >  ./4_report/${ver}/scan_chains_internal.rpt
+report_qor                                      >  ./4_report/${ver}/dft_qor_internal.rpt
+report_area -hierarchy                          >  ./4_report/${ver}/dft_internal_area.rpt
---
+++ 3_DFT/run.csh
@@ -0,0 +1,9 @@
+setenv ver 4_11_2_5ns
+source ../.synopsys_dc.setup
+
+file mkdir ./2_output/${ver}
+file mkdir ./3_log
+file mkdir ./4_report/${ver}
+
+
+dc_shell -64  -f 0_script/DFT_script.tcl       | tee 3_log/${ver}_scan_dc.log
